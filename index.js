96b7ee3ff1a9de9e9b600fbcaeca212eb4e0cb59
mahbub1626 <mahbub1626@gmail.com>
2022-11-08 16:18:55 +0600

    local server worked

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('The Traveler Server Runing')
+})
+app.listen(port, (req, res)=>{
+console.log(`the traveler server running on: ${port}`)
+})
