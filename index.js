0fb8fe64164a001b8e5247bcae91dab4ffc30b99
mahbub1626 <mahbub1626@gmail.com>
2022-11-08 23:17:41 +0600

    data get

--- index.js
+++ index.js
@@ -15,24 +15,29 @@ app.use(express.json())
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3sh2wxl.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
 
     try {
-        const productsCollection = client.db('emaJohn').collection('products');
-
+        const servicesCollection = client.db('theTraveler').collection('services');
+        // all data
         app.get('/services', async (req, res) => {
-            
             const query = {};
-            const cursor = productsCollection.find(query);
-            const products = await cursor.toArray();
-            const count = await productsCollection.estimatedDocumentCount();
-            res.send(products);
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
         });
 
-        app.post('/productsByIds', async (req, res) => {
-            
+        app.post('/services', async (req, res) => {
+            // const services = req.body;
+            const services = {
+                title: "Record of a Shriveled Datum",
+                content: "No bytes, no problem. Just insert a document, in MongoDB",
+            }
+            console.log(services)
+            const result = await orderCollection.insertOne(services);
+            res.send(result);
         })
     }
     finally {
@@ -44,9 +49,9 @@ run().catch(err => console.error(err))
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('The Traveler Server Runing')
 })
-app.listen(port, (req, res)=>{
-console.log(`the traveler server running on: ${port}`)
+app.listen(port, (req, res) => {
+    console.log(`the traveler server running on: ${port}`)
 })
