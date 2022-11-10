fc13093cba0a97d834a43e9825779abd799b4c01
mahbub1626 <mahbub1626@gmail.com>
2022-11-10 21:58:54 +0600

    local server added in vercel and connect with client site

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -10,8 +10,7 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASSWORD)
+
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3sh2wxl.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,6 +20,7 @@ async function run() {
 
     try {
         const servicesCollection = client.db('theTraveler').collection('services');
+        const reviewsCollection = client.db('theTraveler').collection('reviews');
         // all data
         app.get('/services', async (req, res) => {
             const query = {};
@@ -28,14 +28,41 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+        // get all reviews
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
+        // spacific data
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        // create service data
         app.post('/services', async (req, res) => {
             const service = req.body;
-           
+
             console.log(service)
             const result = await servicesCollection.insertOne(service);
             res.send(result);
         })
+
+        // create review data
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+
+            console.log(review)
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        })
+
+
     }
     finally {
 
